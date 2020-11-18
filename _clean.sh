dadcc94f450b68389a65814d80a0e73900d8ca37
turingcompl33t <turingcompl33t@gmail.com>
2020-11-18 12:25:55 -0500

    remove add hoc scripts

--- _clean.sh
+++
@@ -1,8 +0,0 @@
-# _clean.sh
-# Clean build artifacts.
-
-find . -type f -name '*.o' -exec rm {} +
-find . -type f -name '*.out' -exec rm {} +
-find . -type f -name '*.dylib' -exec rm {} +
-find . -type f -name '*.so' -exec rm {} +
-find . -type f -name '*.txt' -exec rm {} +
--- _setup.sh
+++
@@ -1,4 +0,0 @@
-# _setup.sh
-# Hacky way to setup development environment.
-
-export UNIX_WORKSPACE="$(pwd)"
